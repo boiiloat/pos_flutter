4c911247d144506d651c0c679cf88c1b5b1728c9
boiiloat <boiiloat168@gmail.com>
2024-03-12 11:54:33 +0700

    update

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
             "POS",
             style: TextStyle(color: Colors.white),
           ),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
           leading: Builder(
             builder: (context) => IconButton(
               onPressed: () {
@@ -48,13 +48,176 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   Expanded(
                     flex: 3,
-                    child: Container(),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://www.ncausa.org/portals/56/Images/AboutCoffee/NCA_Web_Large_coffee-beans-history.png?ver=2018-02-28-115401-897"),
+                            fit: BoxFit.fill),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: const DecorationImage(
+                                image:
+                                    AssetImage('assets/images/logo_image.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          const Column(
+                            children: [
+                              Text(
+                                "Snack And Relax Coffee",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                              Text(
+                                "Type : Restruant / location: Siem Reap ",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                   Expanded(
                     flex: 7,
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-                      color: Colors.orange,
+                    child: SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 20, bottom: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                                const SizedBox(width: 20),
+                                Container(
+                                  height: 110,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      border: Border.all(color: Colors.grey)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
